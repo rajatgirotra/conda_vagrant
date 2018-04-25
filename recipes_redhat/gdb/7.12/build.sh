f29cc851fade8abb90f1e3af76a29b7259ec0a9c
Rajat Girotra <rajatgirotra@yahoo.com>
2018-04-25 23:54:02 +0800

    more

--- recipes_redhat/gdb/7.12/build.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-./configure --prefix=${PREFIX} --with-system-readline
-make
-make -C gdb install
---
+++ recipes_redhat/gdb/8.1/build.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+./configure --prefix=${PREFIX} --with-system-readline --without-guile 
+make
+make -C gdb/doc doxy
+make -C gdb install
+
+# Install doxygen documentation for gdb
+install -d ${PREFIX}/share/doc/gdb-8.1 && rm -rf gdb/doc/doxy/xml && cp -Rv gdb/doc/doxy ${PREFIX}/share/doc/gdb-8.1
