fae35e7f5a94d5c307c8a9de681f768e450a9f2f
Rajat Girotra <rajatgirotra@yahoo.com>
2018-04-26 04:43:36 +0800

    more

--- recipes_redhat/gcc/7.3.0/build.sh
+++ recipes_redhat/gcc/7.3.0/build.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-# Install gcc to its very own prefix.
+# Install gcc  and binutils to its very own prefix.
 # GCC must not be installed to the same prefix as the environment,
 # because $GCC_PREFIX/include is automatically considered to be a
 # "system" header path.
 # That could cause -I$PREFIX/include to be essentially ignored in users' recipes
 # (It would still be on the search path, but it would be in the wrong position in the search order.)
 GCC_PREFIX="$PREFIX/gcc"
-mkdir "$GCC_PREFIX"
+mkdir -p "$GCC_PREFIX"
 
 PKG_VERSION="rajat_internal_GCC_build_on_$(date '+%d-%m-%Y')"
 ln -s "$PREFIX/lib" "$PREFIX/lib64"
@@ -16,14 +16,37 @@ ln -s "$PREFIX/lib" "$PREFIX/lib64"
 # details about the OS this binary was produced with.
 mkdir -p "${PREFIX}/share"
 cat /etc/*-release > "${PREFIX}/share/conda-gcc-build-machine-os-details"
-chmod +x contrib/download_prerequisites
-chmod +x configure
-./contrib/download_prerequisites
-SRC_DIR=$(basename ${PWD})
+
+mkdir binutils_objdir
+cd binutils_objdir
+
+$PWD/../binutils/configure \
+    --prefix="$GCC_PREFIX" \
+    --bindir="$PREFIX/bin" \
+    --datarootdir="$PREFIX/share" \
+    --libdir="$PREFIX/lib" \
+    --enable-gold=yes \
+    --enable-ld=yes \
+    --with-gmp=${PREFIX} \
+    --with-mpfr=${PREFIX} \
+    --with-mpc=${PREFIX} \
+    --with-isl=${PREFIX} \
+    --enable-lto
+
+make
+make install
 cd ..
-mkdir objdir
-cd objdir
-$PWD/../${SRC_DIR}/configure \
+
+# gcc 
+cd gcc
+#chmod +x contrib/download_prerequisites
+chmod +x configure
+#./contrib/download_prerequisites
+
+cd ..
+mkdir gcc_objdir
+cd gcc_objdir
+$PWD/../gcc/configure \
     --prefix="$GCC_PREFIX" \
     --with-pkgversion="$PKG_VERSION" \
     --with-gxx-include-dir="$GCC_PREFIX/include/c++" \
@@ -31,9 +54,20 @@ $PWD/../${SRC_DIR}/configure \
     --datarootdir="$PREFIX/share" \
     --libdir="$PREFIX/lib" \
     --enable-checking=release \
+    --enable-threads=posix \
     --with-tune=generic \
+    --with-gmp=${PREFIX} \
+    --with-mpfr=${PREFIX} \
+    --with-mpc=${PREFIX} \
+    --with-isl=${PREFIX} \
     --disable-multilib \
     --enable-lto \
+    --enable-default-pie \
+    --enable-__cxa_atexit \
+    --with-as=${PREFIX}/bin/as \
+    --with-gnu-as \
+    --with-ld=${PREFIX}/bin/ld \
+    --with-gnu-ld \
     --enable-languages=c,c++,fortran,lto
 make bootstrap-lean
 #make check-gcc
