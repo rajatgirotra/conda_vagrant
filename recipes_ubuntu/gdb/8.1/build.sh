d9cca9758a09179e512f27a816064be2e25326b5
Rajat Girotra <rajatgirotra@yahoo.com>
2018-05-24 17:05:26 +0800

    py

--- recipes_ubuntu/gdb/8.1/build.sh
+++ recipes_ubuntu/gdb/8.1/build.sh
@@ -1,10 +1,19 @@
 #!/usr/bin/env bash
 
 PYTHON="${PREFIX}/bin/python"
-./configure --prefix=${PREFIX} --with-system-readline --without-guile --with-python=${PYTHON}
+export CFLAGS="-I${PREFIX}/include"
+export CXXFLAGS="-I${PREFIX}/include"
+export LDFLAGS="-L${PREFIX}/lib"
+
+SRC_DIR=$(basename $PWD)
+echo "SRC_DIR is ${SRC_DIR}"
+cd ../
+mkdir -vp gdb_objdir
+cd gdb_objdir
+
+../${SRC_DIR}/configure --prefix=${PREFIX} --with-python=${PYTHON}
 make
-#make -C gdb/doc doxy
-make -C gdb install
+make install
 
 # Install doxygen documentation for gdb
 #install -d ${PREFIX}/share/doc/gdb-8.1 && rm -rf gdb/doc/doxy/xml && cp -Rv gdb/doc/doxy ${PREFIX}/share/doc/gdb-8.1
